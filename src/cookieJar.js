2c900d80f346b73f06ba6f4edff69e3a7b698aaf
Alex <lex.2418@gmail.com>
2018-10-25 23:37:29 -0500

    add test

--- src/cookieJar.js
+++ src/cookieJar.js
@@ -1,42 +1,7 @@
 import window from 'global/window'
 import document from 'global/document'
 
-function parseUnit(unit = '') {
-  // remove the s on plural time units e.g. minute-s
-  const timeUnit = unit.replace('s', '')
-  const time = {
-    minute: () => 60 * 1000,
-    hour: () => 60 * time.minute(),
-    day: () => 24 * time.hour(),
-    week: () => 7 * time.day(),
-    month: () => 30 * time.day(),
-    year: () => 365 * time.day(),
-  }
-
-  switch (timeUnit) {
-    case 'year':
-    case 'month':
-    case 'week':
-    case 'day':
-    case 'hour':
-      return time[timeUnit]()
-
-    case 'minute':
-    default:
-      return time.minute()
-  }
-}
-
-function parseExpireDate(expires = '1 minute') {
-  const byWords = expires.split(' ')
-  const [times, unit] = byWords
-  const duration = times * parseUnit(unit)
-  const date = new Date()
-
-  date.setTime(date.getTime() + duration)
-
-  return date.toUTCString()
-}
+import parseExpireDate from './parseExpireDate'
 
 const cookieJar = {
   setItem(key, value = true, { expires, domain = window.location.hostname, path = '/' } = {}) {
